f376ad983b1768a124724c929abe4a912a12eb6b
tymoyato <20670658+tymoyato@users.noreply.github.com>
2024-09-15 16:34:38 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -85,16 +85,6 @@ install_homebrew() {
     fi
 }
 
-# Function to install packages with brew
-install_packages_with_brew() {
-    printf "${YELLOW}Installing packages with Homebrew...${NC}\n"
-    for package in "$@"; do
-        printf "${YELLOW}Installing $package...${NC}\n"
-        brew install "$package"
-    done
-    printf "${GREEN}All packages installed successfully.${NC}\n"
-}
-
 install_git
 install_curl
 check_sudo
@@ -143,4 +133,15 @@ else
     brew --version
 fi
 source ~/.bashrc
+
+# Function to install packages with brew
+install_packages_with_brew() {
+    printf "${YELLOW}Installing packages with Homebrew...${NC}\n"
+    for package in "$@"; do
+        printf "${YELLOW}Installing $package...${NC}\n"
+        brew install "$package"
+    done
+    printf "${GREEN}All packages installed successfully.${NC}\n"
+}
+
 install_packages_with_brew rbenv nvm
