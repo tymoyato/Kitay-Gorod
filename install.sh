1fc9ed76320467ca99bc6388e46150084377981d
tymoyato <20670658+tymoyato@users.noreply.github.com>
2024-09-15 21:26:40 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -132,16 +132,3 @@ else
     printf "${YELLOW}Verifying Homebrew installation...${NC}\n"
     brew --version
 fi
-. ~/.bashrc
-
-# Function to install packages with brew
-install_packages_with_brew() {
-    printf "${YELLOW}Installing packages with Homebrew...${NC}\n"
-    for package in "$@"; do
-        printf "${YELLOW}Installing $package...${NC}\n"
-        brew install "$package"
-    done
-    printf "${GREEN}All packages installed successfully.${NC}\n"
-}
-
-install_packages_with_brew rbenv nvm
