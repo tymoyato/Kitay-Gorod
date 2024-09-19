ef624b410a923c39cf24fd838ddd95bfd968842d
tymoyato <20670658+tymoyato@users.noreply.github.com>
2024-09-19 22:00:26 +0200

    Update sudo.sh

--- sudo.sh
+++ sudo.sh
@@ -1,21 +1,19 @@
 #!/bin/bash
 
-# Get the current username
-CURRENT_USER=$(whoami)
-
-# Check if the user is already in the sudoers file
-if sudo grep -q "^${CURRENT_USER} " /etc/sudoers; then
-  echo "User ${CURRENT_USER} is already in the sudoers file."
-  exit 0
+# Check if the script is run as root
+if [ "$EUID" -ne 0 ]; then
+  echo "Please run as root"
+  exit 1
 fi
 
-# Add the current user to the sudoers file
-echo "${CURRENT_USER} ALL=(ALL:ALL) ALL" | sudo EDITOR='tee -a' visudo
+# Get the current user
+current_user=$(whoami)
 
-# Check if the user was added successfully
-if sudo grep -q "^${CURRENT_USER} " /etc/sudoers; then
-  echo "User ${CURRENT_USER} has been added to the sudoers file."
+# Check if there are any sudo users
+if ! grep -q '^[^#;]' /etc/sudoers; then
+  # Add the current user to the sudoers file
+  echo "$current_user ALL=(ALL:ALL) ALL" >> /etc/sudoers
+  echo "User $current_user added to /etc/sudoers"
 else
-  echo "Failed to add user ${CURRENT_USER} to the sudoers file."
-  exit 1
+  echo "Sudo users already exist. No changes made."
 fi
