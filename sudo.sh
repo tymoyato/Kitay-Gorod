b8e0f2653f0ed90d62ec55e914a829c31225cb10
tymoyato <20670658+tymoyato@users.noreply.github.com>
2024-09-19 22:06:29 +0200

    Update sudo.sh

--- sudo.sh
+++ sudo.sh
@@ -6,14 +6,11 @@ if [ "$EUID" -ne 0 ]; then
   exit 1
 fi
 
-# Get the current user
-current_user=ervin
-
 # Check if there are any sudo users
 if ! grep -q '^[^#;]' /etc/sudoers; then
   # Add the current user to the sudoers file
-  echo "$current_user ALL=(ALL:ALL) ALL" >> /etc/sudoers
-  echo "User $current_user added to /etc/sudoers"
+  echo "ervin ALL=(ALL:ALL) ALL" >> /etc/sudoers
+  echo "User ervin added to /etc/sudoers"
 else
   echo "Sudo users already exist. No changes made."
 fi
