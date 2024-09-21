841eb0a538e573f6a41f9a80759f545f53a8ab6e
tymoyato <20670658+tymoyato@users.noreply.github.com>
2024-09-21 23:05:40 +0200

    Update linux.sh

--- linux.sh
+++ linux.sh
@@ -14,8 +14,26 @@ install_lib() {
     sudo apt install build-essential -y
 }
 
-check_sudo
-install_lib
-install_git
-install_curl
-install_kitty
+#check_sudo
+#install_lib
+#install_git
+#install_curl
+#install_kitty
+
+# Run each function in the background and wait for it to complete before proceeding to the next one
+check_sudo &
+wait
+
+install_lib &
+wait
+
+install_git &
+wait
+
+install_curl &
+wait
+
+install_kitty &
+wait
+
+printf "${GREEN}All installations completed.${NC}\n"
