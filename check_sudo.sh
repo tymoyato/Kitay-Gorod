891c4b7d91ee853b0ec6fa37fa176796dcaf5f56
tymoyato <20670658+tymoyato@users.noreply.github.com>
2024-09-21 23:58:24 +0200

    Update check_sudo.sh

--- check_sudo.sh
+++ check_sudo.sh
@@ -3,20 +3,29 @@
 source ./colors.sh
 source ./os.sh
 
-# Check if the user is in the sudoers file
 check_sudo() {
     printf "${YELLOW}Checking if the user has sudo privileges...${NC}\n"
+
+    # Check if the user has sudo privileges
     if ! sudo -n true 2>/dev/null; then
         printf "${YELLOW}User does not have sudo privileges. Adding user to sudoers...${NC}\n"
+
         if [[ "$OS" == "Linux" ]]; then
             sudo_user=$(whoami)
             sudo_command="echo '$sudo_user ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers"
-            su -c "$sudo_command"
+
+            # Add the user to sudoers
+            if ! su -c "$sudo_command"; then
+                printf "${RED}Failed to add user to sudoers.${NC}\n"
+                return 1
+            fi
         else
             printf "${RED}Unsupported operating system for adding sudo privileges: ${OS}${NC}\n"
-            exit 1
+            return 1
         fi
     else
         printf "${GREEN}User has sudo privileges.${NC}\n"
     fi
+
+    return 0
 }
