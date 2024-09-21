fdd9042081cbc91f561fb9e1c0dd28f18c53c148
Duttoo Ervin <duttoobadree@gmail.com>
2024-09-21 16:26:34 +0200

    homebrew

--- homebrew.sh
+++ homebrew.sh
@@ -3,34 +3,6 @@
 source ./colors.sh
 source ./os.sh
 
-# Check if Homebrew is already installed
-printf "${YELLOW}Checking if Homebrew is already installed...${NC}\n"
-if ! command -v brew &> /dev/null
-then
-    printf "${YELLOW}Homebrew is not installed. Installing Homebrew...${NC}\n"
-    install_homebrew
-else
-    printf "${GREEN}Homebrew is already installed.${NC}\n"
-    # Check if install_packages.sh exists
-    if [[ -f packages.sh ]]; then
-        # Run the script to install other packages
-        printf "${YELLOW}Running the script to install other packages...${NC}\n"
-        bash packages.sh
-    else
-        printf "${RED}Error: packages.sh not found.${NC}\n"
-        exit 1
-    fi
-fi
-
-# Function to check if Homebrew is installed
-check_brew_installed() {
-    if command -v brew &> /dev/null; then
-        return 0
-    else
-        return 1
-    fi
-}
-
 # Function to install Homebrew
 install_homebrew() {
     if [[ "$OS" == "Linux" ]]; then
@@ -55,19 +27,47 @@ install_homebrew() {
     fi
 }
 
-# Check if Homebrew is installed
-if ! check_brew_installed; then
-    printf "${YELLOW}need to source bashrc.${NC}\n"
-    exit 1
+# Check if Homebrew is already installed
+printf "${YELLOW}Checking if Homebrew is already installed...${NC}\n"
+if ! command -v brew &> /dev/null
+then
+    printf "${YELLOW}Homebrew is not installed. Installing Homebrew...${NC}\n"
+    install_homebrew
 else
-    # Ensure the PATH is correctly set
-    printf "${YELLOW}Ensuring the PATH is correctly set...${NC}\n"
-    if [[ "$OS" == "Linux" ]]; then
-        eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-    elif [[ "$OS" == "Darwin" ]]; then
-        eval "$(/opt/homebrew/bin/brew shellenv)"
+    printf "${GREEN}Homebrew is already installed.${NC}\n"
+    # Check if install_packages.sh exists
+    if [[ -f packages.sh ]]; then
+        # Run the script to install other packages
+        printf "${YELLOW}Running the script to install other packages...${NC}\n"
+        bash packages.sh
+    else
+        printf "${RED}Error: packages.sh not found.${NC}\n"
+        exit 1
     fi
-    # Verify Homebrew installation
-    printf "${YELLOW}Verifying Homebrew installation...${NC}\n"
-    brew --version
 fi
+
+# # Function to check if Homebrew is installed
+# check_brew_installed() {
+#     if command -v brew &> /dev/null; then
+#         return 0
+#     else
+#         return 1
+#     fi
+# }
+
+# # Check if Homebrew is installed
+# if ! check_brew_installed; then
+#     printf "${YELLOW}need to source bashrc.${NC}\n"
+#     exit 1
+# else
+#     # Ensure the PATH is correctly set
+#     printf "${YELLOW}Ensuring the PATH is correctly set...${NC}\n"
+#     if [[ "$OS" == "Linux" ]]; then
+#         eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+#     elif [[ "$OS" == "Darwin" ]]; then
+#         eval "$(/opt/homebrew/bin/brew shellenv)"
+#     fi
+#     # Verify Homebrew installation
+#     printf "${YELLOW}Verifying Homebrew installation...${NC}\n"
+#     brew --version
+# fi
