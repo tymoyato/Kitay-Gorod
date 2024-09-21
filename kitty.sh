49076749fd52aa1594e8881307086db54ee26845
tymoyato <20670658+tymoyato@users.noreply.github.com>
2024-09-22 00:04:04 +0200

    Update kitty.sh

--- kitty.sh
+++ kitty.sh
@@ -4,8 +4,21 @@ source ./colors.sh
 source ./os.sh
 
 # Function to install kitty
+#install_kitty() {
+#    printf "${YELLOW}Installing kitty...${NC}\n"
+#    curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
+#    printf "${GREEN}kitty installed successfully.${NC}\n"
+#}
+
 install_kitty() {
     printf "${YELLOW}Installing kitty...${NC}\n"
-    curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
+
+    # Download and run the kitty installer script
+    if ! curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin; then
+        printf "${RED}Failed to install kitty.${NC}\n"
+        return 1
+    fi
+
     printf "${GREEN}kitty installed successfully.${NC}\n"
+    return 0
 }
