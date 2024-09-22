3db1d062e1a54c87bdf8b59c6944f5d36a3218ac
tymoyato <20670658+tymoyato@users.noreply.github.com>
2024-09-22 16:54:03 +0200

    Update brave.sh

--- brave.sh
+++ brave.sh
@@ -1,25 +1,18 @@
 install_brave() {
-  # Check if curl is installed
-  if ! command -v curl &> /dev/null; then
-    printf "${YELLOW}curl is not installed. Installing curl..."
-    sudo apt update
-    sudo apt install -y curl
-  fi
-
   # Add Brave Browser Beta GPG key
   printf "${YELLOW}Adding Brave Browser Beta GPG key..."
-  sudo curl -fsSLo /usr/share/keyrings/brave-browser-beta-archive-keyring.gpg https://brave-browser-apt-beta.s3.brave.com/brave-browser-beta-archive-keyring.gpg
+  sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpgbeta-archive-keyring.gpg
 
   # Add Brave Browser Beta repository
   printf "${YELLOW}Adding Brave Browser Beta repository..."
-  echo "deb [signed-by=/usr/share/keyrings/brave-browser-beta-archive-keyring.gpg] https://brave-browser-apt-beta.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-beta.list
+  echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
 
   # Update package lists
   printf "${YELLOW}Updating package lists..."
   sudo apt update
   # Install Brave Browser Beta
   echo "Installing Brave Browser Beta..."
-  sudo apt install -y brave-browser-beta
+  sudo apt install -y brave-browser
 
-  echo "Brave Browser Beta installed successfully."
+  echo "Brave Browser installed successfully."
 }
