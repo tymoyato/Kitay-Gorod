a747f0523aa73ae68e860f48b4e3262bf77381d9
Ervin Duttoo <duttoobadree@gmail.com>
2024-09-23 21:36:07 +0200

    add nvim

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,12 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-end
+set fish_greeting ""
+
+set -gx TERM xterm-256color
+
+# aliases
+alias ls "ls -p -G"
+alias la "ls -A"
+alias ll "ls -l"
+alias lla "ll -A"
+alias g git
+alias v nvim
+alias zj zellij
