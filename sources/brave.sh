8b9a28999d26a20e2e7577309bc0345b9b9f3904
tymoyato <20670658+tymoyato@users.noreply.github.com>
2024-09-29 16:41:46 +0200

    fix source brave

--- sources/brave.sh
+++ sources/brave.sh
@@ -4,10 +4,8 @@ source_brave() {
   check_curl
   # Add Brave Browser GPG key
   printf "${YELLOW}Adding Brave Browser Beta GPG key..."
-  if ! sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
-    printf "${YELLOW}Failed to add Brave Browser Beta GPG key.${NC}\n"
-    exit 1
-  fi
+  sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+
   # Add Brave Browser repository
   printf "${YELLOW}Adding Brave Browser Beta repository..."
   echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
