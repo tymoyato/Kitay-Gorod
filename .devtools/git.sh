939abd59730c05f5278d865b18544da98d7efc04
tymoyato <20670658+tymoyato@users.noreply.github.com>
2024-10-12 18:12:51 -0500

    add git

--- .devtools/git.sh
+++ .devtools/git.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 setup_git() {
+    git_name=$(yq '.git.user.name' ./.devtools/git.yml)
+    git_email=$(yq '.git.user.email' ./.devtools/git.yml)
     if [ ! -d ".git" ]; then
         git init
         git remote add origin https://github.com/tymoyato/Kitay-Gorod.git
@@ -8,6 +10,9 @@ setup_git() {
     git remote -v
     git fetch origin main
     git reset --hard origin/main
+
+    git config --global user.email "$git_email"
+    git config --global user.name "$git_name"
 }
 
 setup_git
