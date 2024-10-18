3aa4961b4d45b40dbba1b9006706b033308ce87b
tymoyato <20670658+tymoyato@users.noreply.github.com>
2024-10-18 23:36:58 +0200

    Update init.lua

--- dotfiles/nvim/lua/plugins/init.lua
+++ dotfiles/nvim/lua/plugins/init.lua
@@ -50,8 +50,11 @@ return {
   {
     "iamcco/markdown-preview.nvim",
     cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    build = "cd app && yarn install",
+    init = function()
+      vim.g.mkdp_filetypes = { "markdown" }
+    end,
     ft = { "markdown" },
-    build = function() vim.fn["mkdp#util#install"]() end,
   },
   -- Ruby & Rails stuff
   { "tpope/vim-rails", ft = "ruby", lazy = false },
